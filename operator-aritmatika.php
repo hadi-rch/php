33a2bb06a1e8147361dcfff650b968128965ddb1
hadirch <hadirich3@gmail.com>
2024-10-19 11:25:40 +0700

    operator aritmatika

---
+++ operator-aritmatika.php
@@ -0,0 +1,21 @@
+<?php 
+$a = 100;
+$b = 100;
+$c = 10**2;
+
+$res = $a + $b;
+var_dump($res);
+var_dump($c);
+
+$resultNegative = - $res;
+var_dump($resultNegative);
+
+
+$result = 10 + 10;
+var_dump($result);
+
+$result = 100 % 3;
+var_dump($result);
+
+
+
