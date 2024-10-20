847f0addf2e6cfed730caf7c036e8d1fb1916f6e
hadirch <hadirich3@gmail.com>
2024-10-20 09:59:50 +0700

    goto operator (not recomended)

---
+++ goto-operator.php
@@ -0,0 +1,22 @@
+<?php 
+
+// goto a;
+// echo "Hello world" . PHP_EOL;
+
+// a:
+// echo "Hello A" . PHP_EOL;
+
+
+$counter=1;
+
+while(true){
+    echo "Hello while loop" . $counter . PHP_EOL;
+    $counter++;
+
+    if ($counter > 10){
+        goto end;
+    }
+}
+
+end :
+echo "End Loop";
