2ee5f99a97c0703e0afe23b578a8906eb8de1f0a
hadirch <hadirich3@gmail.com>
2024-10-20 09:42:36 +0700

    For Each

---
+++ foreach-dengan-key.php
@@ -0,0 +1,12 @@
+<?php 
+
+$person = [
+    "first_name" => "EKO",
+    "middle_name" => "KURNIAWAN",
+    "last_name" => "KHANNEDY",
+];
+
+foreach($person as $key => $value) {
+    echo "$key : $value".PHP_EOL;
+}
+
---
+++ foreach.php
@@ -0,0 +1,12 @@
+<?php 
+
+//kode tanpa for each
+$names = ["EKO", "KURNIAWAN", "KHANNEDY"];
+// for($i=0; $i<count($names); $i++){
+//     echo "Hello $names[$i]".PHP_EOL;
+// }
+
+foreach($names as $name) {
+    echo "Hello $name".PHP_EOL;
+}
+
