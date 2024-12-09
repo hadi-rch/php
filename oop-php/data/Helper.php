79eadf09fbb3e90a0148949527c2b44007f5f0da
hadirch <hadirich3@gmail.com>
2024-12-09 07:42:26 +0700

    namespace

---
+++ oop-php/data/Helper.php
@@ -0,0 +1,10 @@
+<?php  
+
+namespace Helper; // jika tanpa tanda kurung kurawal maka satu file ini namespace nya helper.
+ function helpME()
+ {
+    echo "HELP ME" . PHP_EOL;
+ }
+
+ const APPLICATION = "Belajar OOP PHP";
+ 
